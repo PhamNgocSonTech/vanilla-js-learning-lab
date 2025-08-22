8dd486ea2155c98aede455e458d52afa2f7a49fc
PhamNgocSonTech <phamngocson7a1@gmail.com>
2025-08-22 17:20:42 +0700

    optimize xmlSendReq function to use Promise

--- promise/main.js
+++ promise/main.js
@@ -1,11 +1,21 @@
-function xmlSendReq(method, url, callback) {
+function xmlSendReq(method, url) {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.open(method, url, true);
     xhr.send();
     xhr.onload = function () {
       if (xhr.status >= 200 && xhr.status < 400) {
-        resolve(xhr.responseText);
+        const contentType = xhr.getResponseHeader("content-type");
+        const isJson = contentType && contentType.includes("application/json");
+        if (isJson) {
+          try {
+            resolve(JSON.parse(xhr.responseText));
+          } catch (error) {
+            reject("Invalid JSON Format");
+          }
+        } else {
+          resolve(xhr.responseText);
+        }
       } else {
         reject("Network Error");
       }
@@ -25,29 +35,27 @@ xmlSendReq("GET", "./partials/footer.html").then((responseText) => {
   footer.innerHTML = responseText;
 });
 
-xmlSendReq("GET", "https://34tinhthanh.com/api/provinces").then(
-  (responseText) => {
-    const response = JSON.parse(responseText);
-    const provinces = response;
-    provinces.forEach((province) => {
-      const item = document.createElement("li");
-      item.textContent = province.name;
-      provincesList.appendChild(item);
-    });
-  }
-);
+xmlSendReq("GET", "https://34tinhthanh.com/api/provinces").then((result) => {
+  const response = result;
+  const provinces = response;
+  provinces.forEach((province) => {
+    const item = document.createElement("li");
+    item.textContent = province.name;
+    provincesList.appendChild(item);
+  });
+});
 
 xmlSendReq("GET", "https://34tinhthanh.com/api/provinces")
-  .then((responseText) => {
-    const provinces = JSON.parse(responseText);
+  .then((result) => {
+    const provinces = result;
     const firstProvince = provinces[0];
     return xmlSendReq(
       "GET",
       `https://34tinhthanh.com/api/wards?province_code=${firstProvince.province_code}`
     );
   })
-  .then((responseText) => {
-    const ward = JSON.parse(responseText);
+  .then((result) => {
+    const ward = result;
     const firstWard = ward[0];
     console.log(firstWard);
   })

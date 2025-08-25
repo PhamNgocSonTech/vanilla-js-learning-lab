98e5805a6fbdae7fc998f2d87bddcf54f7784fd8
PhamNgocSonTech <phamngocson7a1@gmail.com>
2025-08-25 15:32:26 +0700

    add fetch

---
+++ fetch/main.js
@@ -0,0 +1,47 @@
+async function send(method, url) {
+  const res = await fetch(url, { method });
+  if (!res.ok) throw new Error(`HTTP Code:${res.status}`);
+  const type = res.headers.get("content-type");
+  const isJson = type && type.includes("application/json");
+  return isJson ? res.json() : res.text();
+}
+
+const header = document.querySelector(".header");
+const footer = document.querySelector(".footer");
+const provincesList = document.querySelector(".provinces-list");
+
+send("GET", "./partials/header.html").then((responseText) => {
+  header.innerHTML = responseText;
+});
+
+send("GET", "./partials/footer.html").then((responseText) => {
+  footer.innerHTML = responseText;
+});
+
+send("GET", "https://34tinhthanh.com/api/provinces").then((result) => {
+  const response = result;
+  const provinces = response;
+  provinces.forEach((province) => {
+    const item = document.createElement("li");
+    item.textContent = province.name;
+    provincesList.appendChild(item);
+  });
+});
+
+send("GET", "https://34tinhthanh.com/api/provinces")
+  .then((result) => {
+    const provinces = result;
+    const firstProvince = provinces[0];
+    return send(
+      "GET",
+      `https://34tinhthanh.com/api/wards?province_code=${firstProvince.province_code}`
+    );
+  })
+  .then((result) => {
+    const ward = result;
+    const firstWard = ward[0];
+    console.log(firstWard);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
